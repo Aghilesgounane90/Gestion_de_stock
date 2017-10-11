d814f7dc1b15741fc904d0c2e9e4f1d1985785bc
Aghilesgounane90 <aghiles.gounane@outlook.fr>
2017-10-11 20:05:20 +0200

    ajouter les dao service et le fichier bean-config.xml et ajouter le chemin de bean-config dans le fichier applicationContext

---
+++ src/main/java/com/stock/de/services/ILigneCdeFournisseurService.java
@@ -0,0 +1,26 @@
+package com.stock.de.services;
+
+import java.util.List;
+
+import com.stock.de.entites.LigneCdeFournisseur;
+
+public interface ILigneCdeFournisseurService {
+
+    public LigneCdeFournisseur save(LigneCdeFournisseur entity);
+	
+	public LigneCdeFournisseur update(LigneCdeFournisseur entity);
+	
+	public List<LigneCdeFournisseur> selectAll();
+	
+	public List<LigneCdeFournisseur> selectAll(String sortField, String sort);
+	
+	public LigneCdeFournisseur getById(Long id);
+	
+	public void remove(Long id);
+	
+	public LigneCdeFournisseur findOne(String paramName, Object paramValue);
+	
+	public LigneCdeFournisseur findOne(String[] paramNames, Object[] paramValues);
+	
+	public int findCountBy(String paramName, String paramValue);
+}
